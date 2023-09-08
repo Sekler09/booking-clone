5285d8f616ffbde81b37c8421ee2f750bc39f95e
sekler09 <seklertyoma@gmail.com>
2023-09-08 16:36:14 +0300

    feat: getting counts from search params

--- src/pages/searchResultsPage/index.jsx
+++ src/pages/searchResultsPage/index.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
+import { useSearchParams } from 'react-router-dom';
 
 import CalendarInput from 'components/calendarInput';
 import CityInput from 'components/cityInput';
@@ -10,7 +11,16 @@ import { InputsWrapper } from '../mainPage/styled';
 
 export default function SearchResultsPage() {
   const inputs = useSelector(state => state.inputs);
-
+  const [searchParams, setSearchParams] = useSearchParams();
+  useEffect(() => {
+    searchParams.set('city', inputs.city);
+    searchParams.set('to', inputs.dates.to);
+    searchParams.set('from', inputs.dates.from);
+    searchParams.set('children', inputs.counts.children);
+    searchParams.set('adults', inputs.counts.adults);
+    searchParams.set('rooms', inputs.counts.rooms);
+    setSearchParams(searchParams);
+  }, []);
   return (
     <>
       <InputsWrapper>

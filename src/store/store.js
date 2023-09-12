a5b24e6af9616782f66c3ecdc70d2a7a933f1ef6
sekler09 <seklertyoma@gmail.com>
2023-09-12 21:08:44 +0300

    feat: persist state with redux-persist

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,34 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 
 import inputsReducer from './slices/inputsSlice';
 
-export default configureStore({
-  reducer: {
-    inputs: inputsReducer,
-  },
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const reducers = combineReducers({
+  inputs: inputsReducer,
 });
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+export default store;
